b026827f646152d688979ff2ede2f7426b93474d
frannyfra <francescadelaurentis1987@gmail.com>
2019-08-21 10:36:02 +0100

    first challenge

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const userListFromDatabase = require('./../model');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('home', { users: userListFromDatabase });
+  res.render('home', { users: userListFromDatabase});
 });
 
 module.exports = router;
